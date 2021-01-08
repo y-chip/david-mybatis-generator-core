eb3a70116c4df24fd8713185e576feee5de662a6
Yuta Yamamoto <wookiee33@gmail.com>
2021-01-08 22:07:01 +0900

    Swagger3Pluginを追加する。

---
+++ src/main/java/com/yamamoto/yuta/david/plugins/Swagger3Plugin.java
@@ -0,0 +1,33 @@
+package com.yamamoto.yuta.david.plugins;
+
+import org.mybatis.generator.api.IntrospectedColumn;
+import org.mybatis.generator.api.IntrospectedTable;
+import org.mybatis.generator.api.PluginAdapter;
+import org.mybatis.generator.api.dom.java.Field;
+import org.mybatis.generator.api.dom.java.TopLevelClass;
+
+import java.util.List;
+
+public class Swagger3Plugin extends PluginAdapter {
+
+    @Override
+    public boolean validate(List<String> warnings) {
+        return true;
+    }
+
+    @Override
+    public boolean modelFieldGenerated(
+            Field field,
+            TopLevelClass topLevelClass,
+            IntrospectedColumn introspectedColumn,
+            IntrospectedTable introspectedTable,
+            ModelClassType modelClassType
+    ) {
+
+        topLevelClass.addImportedType("io.swagger.v3.oas.annotations.media.Schema");
+
+        field.addAnnotation("@Schema(required = true)");
+
+        return true;
+    }
+}
